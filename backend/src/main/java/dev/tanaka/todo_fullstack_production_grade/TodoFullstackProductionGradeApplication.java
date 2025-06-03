37a6dee323e1fd23e82c05cdf83cbb7c81cd7ccc
TanakaZuze <99540354+TanakaZuze@users.noreply.github.com>
2025-06-03 13:27:33 +0200

    Initial commit
    setup project structure with backend and frontend folders

---
+++ backend/src/main/java/dev/tanaka/todo_fullstack_production_grade/TodoFullstackProductionGradeApplication.java
@@ -0,0 +1,13 @@
+package dev.tanaka.todo_fullstack_production_grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFullstackProductionGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoFullstackProductionGradeApplication.class, args);
+	}
+
+}
